e18116937e9e4180dcf56e380f2a3c314483b6d5
baga gusdiana <baga.gusdiana@len.co.id>
2022-02-13 12:05:08 +0700

    done dynamic property values and text

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import {Component} from "react";
 import {render} from "react-dom";
 
-import MySection from "./MySection";
-import MyButton from "./MyButton";
+
+const array = ['First', 'Second', 'Third'];
+
+const object = {
+    first: 1,
+    second: 2,
+    third: 3
+};
 
 render(
-    <MySection>
-        <MyButton>My Button Text</MyButton>
-    </MySection>,
+    <section>
+        <h1>Array</h1>
+        <ul>
+            {
+                array.map(i => (
+                   <li key={i}>{i}</li>
+                ))
+            }
+        </ul>
+
+        <h1>Object</h1>
+        <ul>
+            {Object.keys(object).map(i => (
+                <li key={i}>
+                    <strong>{i} : </strong>
+                    {object[i]}
+                </li>
+            ))}
+        </ul>
+    </section>,
     document.getElementById('root')
 );
