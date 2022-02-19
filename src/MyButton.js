f9529cb17382fcddcc502688b228554dacad88b3
baga gusdiana <baga.gusdiana@len.co.id>
2022-02-19 18:45:31 +0700

    handler functions

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,9 +1,13 @@
 import {Component} from "react";
 
 export default class MyButton extends Component {
+    onClick() {
+        console.log('clicked');
+    }
+
     render() {
         return (
-            <button>
+            <button onClick={ this.onClick }>
                 {this.props.children}
             </button>
         );
