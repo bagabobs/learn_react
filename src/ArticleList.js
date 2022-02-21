283c1bb514a9a48522b5de8f1f8eff9a9f05c4dd
baga gusdiana <baga.gusdiana@len.co.id>
2022-02-21 21:59:19 +0700

    ini done juga

---
+++ src/ArticleList.js
@@ -0,0 +1,20 @@
+import ArticleItem from "./ArticleItem";
+
+export default function ArticleList({
+    articles,
+    onClickToggle,
+    onClickRemove
+}) {
+    return (
+        <ul>
+            {articles.map( i => (
+                <ArticleItem
+                    key={i.id}
+                    article={i}
+                    onClickToggle={onClickToggle}
+                    onClickRemove={onClickRemove}
+                />
+            ))}
+        </ul>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import {Component} from "react";
 import {render} from "react-dom";
-
-import MySection from "./MySection";
-import MyButton from "./MyButton";
+import MyFeature from "./MyFeature";
+import AddArticle from "./AddArticke";
+import ArticleList from "./ArticleList";
 
 render(
-    <MySection>
-        <MyButton>My Button Text</MyButton>
-    </MySection>,
+    <MyFeature
+        addArticle={({
+            title,
+            summary,
+            onChangeTitle,
+            onChangeSummary,
+            onClickAdd
+        }) => (
+            <AddArticle
+                name="Articles"
+                title={title}
+                summary={summary}
+                onChangeTitle={onChangeTitle}
+                onChangeSummary={onChangeSummary}
+                onClickAdd={onClickAdd}
+            />
+        )}
+        articleList={({
+            articles, onClickToggle, onClickRemove
+        }) => (
+            <ArticleList
+                articles={articles}
+                onClickToggle={onClickToggle}
+                onClickRemove={onClickRemove}
+            />
+        )}
+    />,
     document.getElementById('root')
 );
